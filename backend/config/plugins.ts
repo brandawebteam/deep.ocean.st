306798511b76fb8e5af24c1552227f7dbe9674ba
MaxProk <prokopchuk.maxym@gmail.com>
2025-09-03 15:09:23 +0200

    r2 bucket

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,28 @@
-export default () => ({});
+export default ({ env }: any) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('S3_ACCESS_KEY_ID'),
+            secretAccessKey: env('S3_ACCESS_SECRET'),
+          },
+          endpoint: env('S3_ENDPOINT'),
+          region: env('S3_REGION', 'auto'),
+          forcePathStyle: true,
+        },
+        params: {
+          Bucket: env('S3_BUCKET'),
+        },
+        baseUrl: env('S3_BASE_URL', null),
+        rootPath: env('S3_ROOT_PATH', ''),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
