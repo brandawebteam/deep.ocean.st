9b8fd14d3b602cf0692c571ce69e420af6047252
MaxProk <prokopchuk.maxym@gmail.com>
2025-09-03 15:22:42 +0200

    r2 bucket

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,5 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+export default ({ env }: any) => ({
+  url: env('PUBLIC_URL'),
+  proxy: true,
+  app: { keys: env.array('APP_KEYS', ['k1','k2','k3','k4']) },
 });
